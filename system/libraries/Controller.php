86281addfcb6ccce38b180c58ae8c11e8d1c5fc9
rrebennack <rrebennack@RebennackLaptop>
2010-12-15 20:27:16 -0500

    Just a test

--- system/libraries/Controller.php
+++ system/libraries/Controller.php
@@ -1,4 +1,7 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if ( !defined('BASEPATH') )
+    exit('No direct script access allowed');
 /**
  * CodeIgniter
  *
@@ -12,7 +15,6 @@
  * @since		Version 1.0
  * @filesource
  */
-
 // ------------------------------------------------------------------------
 
 /**
@@ -27,100 +29,101 @@
  * @author		ExpressionEngine Dev Team
  * @link		http://codeigniter.com/user_guide/general/controllers.html
  */
-class Controller extends CI_Base {
+class Controller extends CI_Base
+    {
 
-	var $_ci_scaffolding	= FALSE;
-	var $_ci_scaff_table	= FALSE;
-	
-	/**
-	 * Constructor
-	 *
-	 * Calls the initialize() function
-	 */
-	function Controller()
-	{	
-		parent::CI_Base();
-		$this->_ci_initialize();
-		log_message('debug', "Controller Class Initialized");
-	}
+    var $_ci_scaffolding = FALSE;
+    var $_ci_scaff_table = FALSE;
 
-	// --------------------------------------------------------------------
+    /**
+     * Constructor
+     *
+     * Calls the initialize() function
+     */
+    function Controller()
+        {
+        parent::CI_Base();
+        $this->_ci_initialize();
+        log_message('debug', "Controller Class Initialized");
+        }
 
-	/**
-	 * Initialize
-	 *
-	 * Assigns all the bases classes loaded by the front controller to
-	 * variables in this class.  Also calls the autoload routine.
-	 *
-	 * @access	private
-	 * @return	void
-	 */
-	function _ci_initialize()
-	{
-		// Assign all the class objects that were instantiated by the
-		// front controller to local class variables so that CI can be
-		// run as one big super object.
-		$classes = array(
-							'config'	=> 'Config',
-							'input'		=> 'Input',
-							'benchmark'	=> 'Benchmark',
-							'uri'		=> 'URI',
-							'output'	=> 'Output',
-							'lang'		=> 'Language',
-							'router'	=> 'Router'
-							);
-		
-		foreach ($classes as $var => $class)
-		{
-			$this->$var =& load_class($class);
-		}
+    // --------------------------------------------------------------------
 
-		// In PHP 5 the Loader class is run as a discreet
-		// class.  In PHP 4 it extends the Controller
-		if (floor(phpversion()) >= 5)
-		{
-			$this->load =& load_class('Loader');
-			$this->load->_ci_autoloader();
-		}
-		else
-		{
-			$this->_ci_autoloader();
-			
-			// sync up the objects since PHP4 was working from a copy
-			foreach (array_keys(get_object_vars($this)) as $attribute)
-			{
-				if (is_object($this->$attribute))
-				{
-					$this->load->$attribute =& $this->$attribute;
-				}
-			}
-		}
-	}
-	
-	// --------------------------------------------------------------------
-	
-	/**
-	 * Run Scaffolding
-	 *
-	 * @access	private
-	 * @return	void
-	 */	
-	function _ci_scaffolding()
-	{
-		if ($this->_ci_scaffolding === FALSE OR $this->_ci_scaff_table === FALSE)
-		{
-			show_404('Scaffolding unavailable');
-		}
-		
-		$method = ( ! in_array($this->uri->segment(3), array('add', 'insert', 'edit', 'update', 'view', 'delete', 'do_delete'), TRUE)) ? 'view' : $this->uri->segment(3);
-		
-		require_once(BASEPATH.'scaffolding/Scaffolding'.EXT);
-		$scaff = new Scaffolding($this->_ci_scaff_table);
-		$scaff->$method();
-	}
+    /**
+     * Initialize
+     *
+     * Assigns all the bases classes loaded by the front controller to
+     * variables in this class.  Also calls the autoload routine.
+     *
+     * @access	private
+     * @return	void
+     */
+    function _ci_initialize()
+        {
+        // Assign all the class objects that were instantiated by the
+        // front controller to local class variables so that CI can be
+        // run as one big super object.
+        $classes = array(
+        'config' => 'Config',
+        'input' => 'Input',
+        'benchmark' => 'Benchmark',
+        'uri' => 'URI',
+        'output' => 'Output',
+        'lang' => 'Language',
+        'router' => 'Router'
+        );
 
+        foreach ( $classes as $var => $class )
+            {
+            $this->$var = & load_class($class);
+            }
 
-}
+        // In PHP 5 the Loader class is run as a discreet
+        // class.  In PHP 4 it extends the Controller
+        if ( floor(phpversion()) >= 5 )
+            {
+            $this->load = & load_class('Loader');
+            $this->load->_ci_autoloader();
+            }
+        else
+            {
+            $this->_ci_autoloader();
+
+            // sync up the objects since PHP4 was working from a copy
+            foreach ( array_keys(get_object_vars($this)) as $attribute )
+                {
+                if ( is_object($this->$attribute) )
+                    {
+                    $this->load->$attribute = & $this->$attribute;
+                    }
+                }
+            }
+        }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Run Scaffolding
+     *
+     * @access	private
+     * @return	void
+     */
+    function _ci_scaffolding()
+        {
+        if ( $this->_ci_scaffolding === FALSE OR $this->_ci_scaff_table === FALSE )
+            {
+            show_404('Scaffolding unavailable');
+            }
+
+        $method = (!in_array($this->uri->segment(3), array('add', 'insert', 'edit', 'update', 'view', 'delete', 'do_delete'), TRUE)) ? 'view' : $this->uri->segment(3);
+
+        require_once(BASEPATH . 'scaffolding/Scaffolding' . EXT);
+        $scaff = new Scaffolding($this->_ci_scaff_table);
+        $scaff->$method();
+        }
+
+    }
+
 // END _Controller class
 
 /* End of file Controller.php */
