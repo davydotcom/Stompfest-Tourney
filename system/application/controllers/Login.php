8b4844a77e9a69f0c2c26f3f57470302f4e9f81e
unknown <rrebennack@.(none)>
2010-12-20 02:06:29 -0500

    Started login: Code will not run.

---
+++ system/application/controllers/Login.php
@@ -0,0 +1,29 @@
+<?php
+
+class Login extends Controller
+    {
+    function index()
+        {
+        }
+
+    function Validate()
+        {
+		$this->load->model("membership_model");
+
+		$xQuery = $this->membership_model->validate();
+
+		if($query) // if the user's credentials validated...
+		{
+			$data = array(
+				'username' => $this->input->post('username'),
+				'is_logged_in' => true
+			);
+			$this->session->set_userdata($data);
+			redirect('site/members_area');
+		}
+		else // incorrect username or password
+		{
+			$this->index();
+		}
+        }
+    }
