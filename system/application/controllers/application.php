86bc6984d38d5aef4b78cab0e260009dcde4c307
unknown <rrebennack@.softwaresysinc.lan>
2011-01-08 13:32:41 -0500

    Pretty Formatting :(

--- system/application/controllers/application.php
+++ system/application/controllers/application.php
@@ -2,33 +2,31 @@
 
 class ApplicationController extends Controller
     {
-    function  __construct()
+
+    function __construct()
         {
-                
-   		parent::Controller();
-                
-                
-                
-                //Filters That Run on Every Action
-                $this->loadCurrentGamer();
+
+        parent::Controller();
+
+
+
+        //Filters That Run on Every Action
+        $this->loadCurrentGamer();
         }
 
-
-
     function loadCurrentGamer()
-    {
+        {
         $this->load->model("Gamer");
         $handle = $this->session->userdata('Handle');
-                $this->mysmarty->assign('isLoggedIn', false);
-        if($handle != null)
+        $this->mysmarty->assign('isLoggedIn', false);
+        if ( $handle != null )
+            {
+            $this->currentGamer = $this->Gamer->findByHandle($handle);
+            if ( $this->currentGamer != null )
                 {
-                    $this->currentGamer = $this->Gamer->findByHandle($handle);
-                    if($this->currentGamer != null)
-                    {
-                        $this->mysmarty->assign('isLoggedIn', true);
-                    }
+                $this->mysmarty->assign('isLoggedIn', true);
                 }
-    }
-
+            }
+        }
 
     }
