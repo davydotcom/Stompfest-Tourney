1df3ab0b7cdaf647d53dea3295854d20c2e62524
David <destes@redwindsw.com>
2011-01-08 16:24:13 -0500

    adding more namespaces

---
+++ system/application/controllers/tourneys/application.php
@@ -0,0 +1,13 @@
+<?php
+require_once(APPPATH . "/controllers/application.php");
+class TourneyApplicationController extends ApplicationController
+    {
+    function  __construct()
+        {
+   		parent::__construct();
+
+   
+        }
+
+
+    }
