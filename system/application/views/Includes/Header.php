a9c1ebc44cce5ea1d019348d0d5c0df7af63fbe1
unknown <rrebennack@.softwaresysinc.lan>
2011-01-08 13:37:01 -0500

    removed login

--- system/application/views/Includes/Header.php
+++
@@ -1,8 +0,0 @@
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Stompfest: Tournament Manager</title>
-
-    <link rel="stylesheet" href="/CSS/Main.css" type="text/css" media="screen" />
-</head>
-<body>
