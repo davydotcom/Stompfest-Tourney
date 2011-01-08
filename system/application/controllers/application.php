2ff5b0bfa5e2185d9089dba47973be8ce5a93208
David <destes@redwindsw.com>
2011-01-08 14:15:01 -0500

    whoops

--- system/application/controllers/application.php
+++ system/application/controllers/application.php
@@ -19,12 +19,15 @@ class ApplicationController extends Controller
         $this->load->model("User");
         $userID = $this->session->userdata('userID');
         $this->mysmarty->assign('isLoggedIn', false);
-        if ( $handle != null )
+        $this->isLoggedIn = false;
+
+        if ( !empty($userID) )
             {
             $this->currentUser = $this->User->findByUserID($userID);
-            if ( $this->currentUser != null )
+            if ( !empty($this->currentUser))
                 {
-                $this->mysmarty->assign('isLoggedIn', true);
+                    $this->mysmarty->assign('isLoggedIn', true);
+                    $this->isLoggedIn = true;
                 }
             }
         }
