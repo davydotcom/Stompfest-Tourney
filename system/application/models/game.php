6b5038ec3aa3e450117b55bb5dda68893752d583
David <destes@redwindsw.com>
2011-01-09 15:05:52 -0500

    created extended SFModel class with added features

--- system/application/models/game.php
+++ system/application/models/game.php
@@ -4,14 +4,7 @@ require_once(APPPATH . "/models/sfmodel.php");
 class Game extends SFModel
 {
 
-    function findGames($options = array())
-    {
-        $this->applyOptions($options);
-
-        $query = $this->db->get($this->tableName);
-
-        return $query->result();
-    }
+   
 
 
 
