37813f723bd2915882ebcec64baffe8c71413531
David <destes@redwindsw.com>
2011-03-24 14:19:17 -0400

    here we go again

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -38,9 +38,9 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "";
-$db['default']['password'] = "";
-$db['default']['database'] = "";
+$db['default']['username'] = "stompfest";
+$db['default']['password'] = "imn7734";
+$db['default']['database'] = "sftourney";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
